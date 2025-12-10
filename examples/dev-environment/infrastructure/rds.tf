ed4e23e0bc37a841682f7a05aff3eac7f33fe40d
akileshthuniki <thunikiakilesh@gmail.com>
2025-12-10 05:54:52 -0700

    Set create_random_password to false to use provided password

--- examples/dev-environment/infrastructure/rds.tf
+++ examples/dev-environment/infrastructure/rds.tf
@@ -17,6 +17,7 @@ module "rds" {
   db_name  = "taskdb"
   username = "taskuser"
   password = var.db_password
+  create_random_password = false  # Use provided password instead of random
   
   vpc_id      = module.vpc.vpc_id
   subnet_ids  = module.vpc.private_subnet_ids
